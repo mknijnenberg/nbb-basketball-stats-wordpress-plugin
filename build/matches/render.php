6fba03677e79b54e3ba97409a5a569fc48fe4242
Maarten Knijnenberg <maarten@myparcel.nl>
2025-03-30 21:44:48 +0200

    update styling and setup for the matches

--- build/matches/render.php
+++ build/matches/render.php
@@ -24,12 +24,23 @@ $matches = Utils::get_cached_external_json('http://db.basketball.nl/db/json/weds
 				<div class="matches__title"><?php echo $matches['naam']; ?></div>
 
 				<table class="matches__table">
+					<thead class="matches__table-header-group">
+						<tr>
+							<th class="matches__table-header">Datum</th>
+							<th class="matches__table-header">Team thuis</th>
+							<th class="matches__table-header">Uitslag</th>
+							<th class="matches__table-header">Team uit</th>
+							<th class="matches__table-header">Locatie</th>
+						</tr>
+					</thead>
+
 					<tbody>
 						<?php foreach ($matches['wedstrijden'] as $game) :
 						$date = new DateTime($game['datum']);
 
 						$game_played = $game['score_thuis'] || $game['score_uit'];
-						$home
+						$homeWon = $game['score_thuis'] > $game['score_uit'];
+						$awayWon = $game['score_uit'] > $game['score_thuis'];
 							?>
 							<tr
 								class="matches__table-row"
@@ -41,8 +52,7 @@ $matches = Utils::get_cached_external_json('http://db.basketball.nl/db/json/weds
 							>
 								<td class="matches__table-column">
 									<div class="matches__table-column--date">
-										<span><?php echo $date->format('d-m-Y'); ?></span>
-										<span><?php echo $date->format('H:i'); ?> uur</span>
+										<?php echo $date->format('d-m-Y H:i'); ?>
 									</div>
 								</td>
 
@@ -61,19 +71,6 @@ $matches = Utils::get_cached_external_json('http://db.basketball.nl/db/json/weds
 
 											<div><?php echo $game['thuis_ploeg']; ?></div>
 										</div>
-
-										<div
-											class="matches__table-column--team"
-											data-away-team
-											data-away-team-id="<?php echo $game['uit_ploeg_id']; ?>"
-											data-away-club-id="<?php echo $game['uit_club_id']; ?>"
-										>
-											<img
-												src="<?php echo $game['logo_uit']; ?>"
-												alt="<?php echo $game['uit_ploeg']; ?>"
-											/>
-											<?php echo $game['uit_ploeg']; ?>
-										</div>
 									</div>
 								</td>
 
@@ -85,11 +82,27 @@ $matches = Utils::get_cached_external_json('http://db.basketball.nl/db/json/weds
 											data-home-team-score="<?php echo $game['score_thuis']; ?>"
 											data-away-team-score="<?php echo $game['score_uit']; ?>"
 										>
-											<div class="matches__table-column--score"><?php echo $game['score_thuis']; ?></div>
-
-											<div><?php echo $game['score_uit']; ?></div>
+											<div class="matches__table-column--score <?php echo $homeWon ? 'matches__table-column--score--winner' : '';?>"><?php echo $game['score_thuis']; ?></div>
+											<div>-</div>
+											<div class="matches__table-column--score <?php echo $awayWon ? 'matches__table-column--score--winner' : '';?>"><?php echo $game['score_uit']; ?></div>
 										</div>
 									<?php } ?>
+								</td>
+
+								<td class="matches__table-column">
+									<div
+										class="matches__table-column--team matches__table-column--team--away"
+										data-away-team
+										data-away-team-id="<?php echo $game['uit_ploeg_id']; ?>"
+										data-away-club-id="<?php echo $game['uit_club_id']; ?>"
+									>
+										<img
+											src="<?php echo $game['logo_uit']; ?>"
+											alt="<?php echo $game['uit_ploeg']; ?>"
+										/>
+
+										<div><?php echo $game['uit_ploeg']; ?></div>
+									</div>
 								</td>
 
 								<td class="matches__table-column">
@@ -101,8 +114,10 @@ $matches = Utils::get_cached_external_json('http://db.basketball.nl/db/json/weds
 										data-lon="<?php echo $game['loc_lon']; ?>"
 									>
 										<span><?php echo $game['loc_naam']; ?></span>
+
 										<span><?php echo $game['loc_plaats']; ?></span>
 									</div>
+								</td>
 							</tr>
 						<?php endforeach; ?>
 					</tbody>
